62cbf142cc49d0d474a4d613dd8d3fe95eb616bc
Phydrin <84876160+Phydrin@users.noreply.github.com>
2022-09-17 17:28:40 +0100

    Added email popup, counter, testimonials & images

---
+++ Team2 Project/popup.js
@@ -0,0 +1,7 @@
+var input = prompt("Would you like to join our mailing list?")
+if(input=="yes"){
+    alert("You're signed up! Check your email for verification.")
+  }
+  else {
+    alert ("Ok, you can change your mind in Settings.")
+  }
