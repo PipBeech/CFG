62c0a7ca7ba34e087f8759eeed13e7fe3f204560
Phydrin <84876160+Phydrin@users.noreply.github.com>
2022-09-30 16:53:36 +0100

    created new popup, removed old popup

--- Team2 Project/popup.js
+++
@@ -1,7 +0,0 @@
-var input = prompt("Would you like to join our mailing list?")
-if(input=="yes"){
-    alert("You're signed up! Check your email for verification.")
-  }
-  else {
-    alert ("Ok, you can change your mind in Settings.")
-  }
