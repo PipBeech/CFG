0c083e928670d4aa21652bc473995e4237f42371
Pip Beech <pipbeech@me.com>
2022-10-03 21:22:54 +0100

    Polished up to match figma

--- Team2 Project/BaseJava.js
+++ Team2 Project/BaseJava.js
@@ -1,1 +1,10 @@
-<button onclick="location.href='BaseCode.html'">Home</button>
+function signMeUp() {
+    let email = prompt("Enter your email address to join our mailing list!", "Enter your email address...")
+    let text;
+        if (email == null || email == "") {
+            text = "Ok, you can join the list later in Settings."
+        }
+        else {
+            text = "We have sent a verification email to " + email;
+        }
+}
